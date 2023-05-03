a34978bed846e4f3c724f5e126dd00659500da78
fixer400 <fixer400@yandex.ru>
2023-05-03 08:47:29 +0300

    Bug fix

--- FinalParser.py
+++ FinalParser.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from multiprocessing import freeze_support
 from multiprocessing import Pool
 import re
 
@@ -25,7 +26,7 @@ def appendJson(entry):
 
     # Verify existing list
     print(type(listObj))
-    
+
     listObj.append(entry)
     
     # Verify updated list
@@ -40,7 +41,7 @@ def appendJson(entry):
 
 def findImgByName(name, array):
     for i, value in enumerate(array):
-        if value['Название'] == name:
+        if value['Наименование'] == name:
             return i
     return False
 
@@ -49,44 +50,37 @@ def verificateFile():
     with open('sample.json', "r", encoding='utf8') as outfile:
         foundImg = json.load(outfile)
     for i in foundImg:
-        result = findImgByName(i['Название'], dataArr)
+        result = findImgByName(i['Наименование'], dataArr)
         if result != -1:
             del dataArr[result]
 
-def findImg():
-    retries = 0
-    count = 1
-    
-    while True:
-        driver.implicitly_wait(3)
-        imgList = driver.find_element(By.XPATH, '//div[@jsname="r5xl4"]/div['+str(count)+']')
+def LoadImg():
+    for count in range (1,10):
+        count = str(count)
+        imgList = driver.find_element(By.XPATH, '//*[@id="islrg"]/div[1]/div['+count+']')
         imgList.click()
-        path = driver.find_element(By.CLASS_NAME,'zjoqD')
-        driver.implicitly_wait(3)
-        imgSrc = path.find_element(By.TAG_NAME,'img')
-        for retries in range (3):
-            if(imgSrc.get_attribute('src').startswith("data")):
-                time.sleep(2)
-                print(retries)
-            else:
-                return imgSrc
-        count = count + 1
-
+        time.sleep(4)
+        path = driver.find_element(By.XPATH,'//*[@id="Sva75c"]/div[2]/div/div[2]/div[2]/div[2]/c-wiz/div/div/div/div[3]/div[1]/a/img[1]')
+        if (path.get_attribute('src').startswith("data") == False):
+            return path.get_attribute('src')
 
 def main(i):
-    url = 'https://www.google.com/search?q=+'+i['Название'].replace(" ","+")+'&hl=ru&prmd=ivmn&sxsrf=AJOqlzVHmngGsZrz__M8PHnLQWxQhXtFhQ:1675148751745&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiOsqyyn_H8AhUqiv0HHUmfB0EQ_AUoAXoECAEQAQ&biw=1018&bih=850&dpr=2'
+    url = 'https://www.google.com/search?q=+'+i['Наименование'].replace(" ","+")+'&hl=ru&prmd=ivmn&sxsrf=AJOqlzVHmngGsZrz__M8PHnLQWxQhXtFhQ:1675148751745&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiOsqyyn_H8AhUqiv0HHUmfB0EQ_AUoAXoECAEQAQ&biw=1018&bih=850&dpr=2'
     driver.get(url=url)
-    driver.implicitly_wait(3)
-    imgSrc = findImg()
+    time.sleep(1)
+    imgSrc = LoadImg()
     dictionary = {
-        "Название":i["Название"],
-        "Ссылка":imgSrc.get_attribute('src')
+        "Наименование":i["Наименование"],
+        "Ссылка":imgSrc
     }
     appendJson(dictionary)
 
 if __name__ == '__main__':
+    freeze_support()
     verificateFile()
     print('Введите сколько браузеров открыть для парсинга (от 1 до 5)')
+    #p = Pool(processes = 1)
+    #p.map(main, dataArr)
     x = input()
     if int(x) <= 5:
         p = Pool(processes = int(x))
